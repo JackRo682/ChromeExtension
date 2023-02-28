5bfec1412fb05dba6002e687d1b956b86698d7fe
JackRo682 <60928974+JackRo682@users.noreply.github.com>
2023-02-28 21:50:33 +0900

    대표 이미지, manifest 변경
    로고 이미지, manifest 파일을 변경하고 <span> 태그에 결과를 대체하는 코드도 작성했다. 정상적으로 작동중임

--- Chrome Extension/js/StringExtract.js
+++ Chrome Extension/js/StringExtract.js
@@ -2,34 +2,61 @@
 const productNameCells = document.querySelectorAll('td[data-original-value]');
 
 // Extract the data from each <td> element and store it in an array
-const productNames = Array.from(productNameCells).map(td => td.getAttribute('data-original-value'));
+const productNames = Array.from(productNameCells).map((td, index) => {
 
-// Find all the <td> elements with the "class" attribute
-const priceCells = document.querySelectorAll('td.right-align');
+  // 제목 추출
+  const productName = td.getAttribute('data-original-value');
+  const productNumeric = productName.match(/^\d+(?:\.\d+)?/)[0];
 
-// Extract the data from each <td> element and store it in an array
-const prices = [];
-for (let i = 0; i < priceCells.length; i += 2) {  
-  const priceElement = priceCells[i].querySelector('span');
-  if (priceElement) {
-    prices.push(priceElement.textContent);
-  }
-}
-
-// Extract only the numeric part of the price
-const numericPrices = prices.map(price => price.replace(/[^0-9]/g, ''));
-
-
-// Iterate over the arrays and log the values
-for (let i = 0; i < productNames.length; i++) {
-  // Extract the numeric part of the first element in the productName string
-  const productNumeric = productNames[i].match(/^\d+(?:\.\d+)?/)[0];
-
-  // Multiply by 10000 and 1.1, and add 10000 to get the final price
+  // 최저 판매 목표가 계산
   const productPrice = Math.round((productNumeric * 10000 * 1.1) + 10000);
 
-  const margin = (numericPrices[i] - productPrice) + 10000;
+  // 판매가 추출
+  const priceCells = document.querySelectorAll('td.right-align');
+  const prices = [];
 
-  // Log the product name and its corresponding price
-  console.log(`${productPrice} \t ${numericPrices[i]} \t ${margin}`);
-}
+  for (let i = 0; i < priceCells.length; i += 2) {  
+    const priceElement = priceCells[i].querySelector('span');
+    if (priceElement) {
+      prices.push(priceElement.textContent);
+    }
+  }
+
+  // 마진 계산하고 수정된 string 생성  
+  const numericPrice = prices[index].replace(/[^0-9]/g, '');
+  const margin = (numericPrice - productPrice) + 10000;
+  const formattedMargin = (margin / 10000).toFixed(2);
+  const productNameWithoutNumeric = productName.replace(productNumeric+"만원", '').trim();
+  const newProductName = `${productNumeric}만원/${formattedMargin} - ${productNameWithoutNumeric}`;
+
+  // Output modified product name to console
+  console.log(newProductName);
+
+  return newProductName;
+});
+
+
+
+
+// Update the product name cells in the table
+const productNameCellsToUpdate = document.querySelectorAll('td[data-original-value]');
+productNameCellsToUpdate.forEach((cell, index) => {
+  const productNameDiv = cell.querySelector('div.text-wrapper');
+  productNameDiv.querySelector('span').textContent = productNames[index];
+});
+
+
+
+
+// Find all the rows in the table body
+const tableRows = document.querySelectorAll('table > tbody > tr');
+
+// Loop through each row and check if formattedMargin is negative
+tableRows.forEach((row, index) => {
+  const marginCell = row.querySelector(`td:nth-child(5) > div > span`);
+  const formattedMargin = parseFloat(marginCell.textContent.replace(/[^\d.-]/g, ''));
+
+  if (formattedMargin < 0) {
+    row.style.backgroundColor = '#faaaaa'; // set the background color to light red
+  }
+});
